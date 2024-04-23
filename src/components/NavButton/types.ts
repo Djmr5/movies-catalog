e3aef72828a811193ef6dbe4fdffb0c350ff4c75
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-04-23 12:27:07 -0600

    NavButton component

---
+++ src/components/NavButton/types.ts
@@ -0,0 +1,14 @@
+export interface IButton {
+    /**
+     * The text of the button.
+     */
+    text: string;
+    /**
+     * The color of the button.
+     */
+    color: 'red' | 'blue' | 'green' | 'yellow' | 'purple';
+    /**
+     * The link of the button.
+     */
+    link: string;
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export { MovieCard } from './MovieCard'
+export { Pill } from './Pill'
+export { Header } from './Header'
+export { NavButton } from './NavButton'
